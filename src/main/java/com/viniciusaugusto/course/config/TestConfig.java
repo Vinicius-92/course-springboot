58ce2ba51a1f96e9297ccb7e290ababa877ea27e
vinicius-92 <vinicius_augusto_@msn.com>
2021-01-31 13:17:53 -0300

    Payment, one-to-one association

--- src/main/java/com/viniciusaugusto/course/config/TestConfig.java
+++ src/main/java/com/viniciusaugusto/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.viniciusaugusto.course.entities.Category;
 import com.viniciusaugusto.course.entities.Order;
 import com.viniciusaugusto.course.entities.OrderItem;
+import com.viniciusaugusto.course.entities.Payment;
 import com.viniciusaugusto.course.entities.Product;
 import com.viniciusaugusto.course.entities.User;
 import com.viniciusaugusto.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
